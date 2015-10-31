9961f9f9d356ca50e63afde82f8f31e4dff05f91
faisalnet5 <faisalnet5@gmail.com>
2015-10-30 21:16:37 -0400

    Create TestOverride.java

---
+++ TestOverride.java
@@ -0,0 +1,31 @@
+package com.java.override;
+
+class ObjectA{
+	
+	//Element-1: Variable(s)
+	String greeting = "Welcome";
+	
+	//Element-2: Methods(s)
+	void objectFunction(){
+		System.out.println("Hello from Object A");
+	}
+}
+
+class ObjectB extends ObjectA{
+	
+	//Element-2: Method(s)
+	void objectFunction(){
+		System.out.println("Hello from Object B");
+	}
+}
+
+public class TestOverride {
+
+	public static void main(String[] args) {
+		
+		ObjectB copyObject = new ObjectB();
+		copyObject.objectFunction();
+		System.out.println(copyObject.greeting);
+		
+	}
+}
